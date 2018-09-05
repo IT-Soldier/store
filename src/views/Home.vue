77e3eb71d8221f8b8ea194b5de3687e1acb2ffde
hanchao <1650070770@qq.com>
2018-09-06 01:08:06 +0800

    登出功能与防跳墙机制

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>Header
+      <a href="javascript:;" @click="logout">退出</a>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -10,7 +12,25 @@
 
 <script>
 export default {
-
+  beforeCreate() {
+    // 在页面加载前,就进行token的验证,效果类似axios的同步请求
+    const token = sessionStorage.getItem('token');
+    if(!token) {
+    // 仅仅验证了token是否存在,若有token,即使不正确也可以看到Home页面,但是无数据渲染,
+    // 前端的验证强度已经足够
+    this.$router.push('/Login');
+    this.$message.warning('请登录后访问页面!');
+    }
+  },
+  methods: {
+    logout() {
+      // 清空sessionStorage的token
+      sessionStorage.removeItem('token');
+      // 跳转到登录页,并提醒登录
+      this.$router.push('/Login');
+      this.$message.success('您已经成功退出!');
+    }
+  }
 };
 </script>
 
