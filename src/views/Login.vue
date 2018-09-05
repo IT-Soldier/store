1d4cbce15ec32b0153a6b82f1c8d98fc8c5b5c65
hanchao <1650070770@qq.com>
2018-09-05 22:18:19 +0800

    跳转成功/失败,消息提醒

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,6 +10,7 @@
     </el-form-item>
     <el-button class="login-button" type="primary" @click="handleLogin">登录按钮</el-button>
   </el-form>
+  
 </div>
 </template>
 
@@ -36,12 +37,14 @@ export default {
           let {msg, status} = response.data.meta;
           if (status === 200) {
             console.log(response.data);
+            // 提供的消息框,由element-ui提供
+             this.$message.success(msg);
             // 使用的是$router.push, 而不是文档中router.push,
             // 是因为router插件为了全局使用, webpack已经暗中在vue中注册了router插件
-            // 所以用法同axios的封装使用
+            // 所以用法同axios的封装使用,它叫编程导航
             this.$router.push('/Home');
           } else {
-            console.log(msg);
+            this.$message.error(msg);
           }
         })
         .catch(error => (
