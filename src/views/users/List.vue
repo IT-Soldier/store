3e596972cff65e8dd5ead9bbe6641619b78f7b5e
hanchao <1650070770@qq.com>
2018-09-09 21:33:39 +0800

    添加功能完成

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -11,18 +11,52 @@
     <div style="margin-top: 15px;">
       <!-- clearable使得输入框可以被清除 -->
       <el-input
-      clearable
-      placeholder="请输入内容"
-      class="input-with-select"
-      v-model="query">
+        clearable
+        placeholder="请输入内容"
+        class="input-with-select"
+        v-model="query">
         <!-- slot="append"使得搜索按钮存在 -->
         <el-button
-        slot="append"
-        icon="el-icon-search"
-        @click="handelQuery"></el-button>
+          slot="append"
+          icon="el-icon-search"
+          @click="handelQuery"></el-button>
       </el-input>
-      <el-button type="success" plain>添加用户</el-button>
+      <el-button
+        type="success"
+        plain
+        @click="dialogFormVisible = true">添加用户</el-button>
     </div>
+    <!-- 添加用户弹出框 -->
+    <!-- close是Dialog 关闭的回调函数 -->
+    <el-dialog
+      center
+      @close="closeDialog"
+      :visible.sync="dialogFormVisible">
+      <el-form
+      label-position="right"
+      label-width="120px"
+      :rules="rules"
+      :model="formData"
+      ref="ruleForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input type="text" v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input type="email" v-model="formData.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input type="text" v-model="formData.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="quitDialog">取 消</el-button>
+        <el-button type="primary" @click="handelAdd">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 表格主体 -->
     <!-- 当el-table元素中注入data对象数组后，
     在el-table-column中用prop属性来对应对象中的键名即可填入数据，
     用label属性来定义表格的列名。
@@ -77,12 +111,17 @@
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
           <el-button size="mini" type="primary" icon="el-icon-edit" plain></el-button>
-          <el-button size="mini" type="success" icon="el-icon-check" plain></el-button>
-          <el-button size="mini" type="danger" icon="el-icon-delete" plain
+          <el-button
+          size="mini"
+          type="danger"
+          icon="el-icon-delete"
+          plain
           @click="handelDelele(scope.row.id)"></el-button>
+          <el-button size="mini" type="success" icon="el-icon-check" plain></el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页导航 -->
     <el-pagination
       class="fenye"
       @size-change="handleSizeChange"
@@ -102,7 +141,24 @@ export default {
     return {
       tableData: [],
       query: '',
-      currentPage4: 4
+      currentPage4: 4,
+      dialogFormVisible: false,
+      formData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 11, message: '长度在 6 到 11 个字符', trigger: 'blur' }
+        ]
+      }
     };
   },
   methods: {
@@ -175,6 +231,57 @@ export default {
         });
       });
     },
+    // 添加功能
+    handelAdd() {
+      this.dialogFormVisible = true;
+      this.$refs.ruleForm.validate((valid) => {
+      // 对整个表单进行校验的方法，参数为一个回调函数。
+      // 该回调函数会在校验结束后被调用，并传入两个参数：是否校验成功和未通过校验的字段。
+      // 若不传入回调函数，则会返回一个 promise
+      // 因为它封装了promise对象,取值方式相同,valid经过检验是验证之后返回的布尔值
+      // console.log(valid);
+      if (!valid) {
+        return;
+      }
+      this.$http
+        .post(`users`,this.formData)
+        .then(response => {
+          const {msg, status} = response.data.meta;
+          // 用post时相对比较特殊,标识码是201
+          if(status === 201) {
+            // 添加成功关闭弹出框
+            this.dialogFormVisible = false;
+            // 重新加载数据,刷新页面
+            this.loadData();
+            // 清空文本框
+            for(let key in this.formData) {
+              this.formData[key] = '';
+            }
+            this.$message.success('添加成功');
+          } else {
+            this.$message.error(msg);
+          }
+        })
+        .catch(error => {
+          this.$message.error(error);
+        })
+      });
+    },
+    // 取消按钮,关闭弹出框
+    quitDialog() {
+      this.dialogFormVisible = false;
+      // 清空表单
+      for(let key in this.formData) {
+        this.formData[key] = '';
+      }
+    },
+    // 叉号关闭弹出框
+    closeDialog() {
+       // 清空表单
+      for(let key in this.formData) {
+        this.formData[key] = '';
+      }
+    },
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`);
     },
@@ -196,7 +303,7 @@ export default {
 .box-card {
   height: 100%;
   /* card高度不够,默认是hidden */
-  overflow: auto;
+  overflow: hidden;
 }
 .el-input {
   width: 400px;
@@ -211,5 +318,8 @@ export default {
 }
 .el-pagination__jump {
   margin-top: -10px;
+}
+.el-form-item__label {
+  margin-top: 10px;
 }
 </style>
